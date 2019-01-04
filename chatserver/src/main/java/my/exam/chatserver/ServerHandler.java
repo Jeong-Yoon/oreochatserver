0c83f5442ec74fbfc4c78af11c765f64615476ca
jeong <jjung4025@naver.com>
2019-01-04 12:23:10 +0900

    수정하면 좋을 이슈 작성.

--- chatserver/src/main/java/my/exam/chatserver/ServerHandler.java
+++ chatserver/src/main/java/my/exam/chatserver/ServerHandler.java
@@ -28,11 +28,21 @@ public class ServerHandler implements Runnable {
             while (true) {
                 String message = chatUser.read();
                 System.out.println(chatUser.getNickname() + "님의 입력 : " + message);
+                /**
+                 * create를 할 때 방 제목을 입력하지 않은 경우
+                 * 띄어쓰기를 하지 않고 엔터를 누르면 방 제목이 /create로 들어가는 문제가 있습니다.
+                 *
+                 * join을 할 때 방 번호를 입력하지 않은 경우 프로그램이 죽는 문제를 해결해주세요.
+                 *
+                 * quit을 했을 때 클라이언트를 종료해주세요.
+                 */
                 if (!inRoom) { // 로비에 있을 경우
                     if (message.indexOf("/create") == 0) {
                         String title = message.substring(message.indexOf(" ") + 1);
                         chatLobby.createRoom(chatUser, title, true);
                         inRoom = true;
+                        //방 생성 후 성공했다는 문구를 남겼습니다.
+                        System.out.println("방이 생성되었습니다.");
 
                     }else if(message.indexOf("/join") == 0){
                         String strRoomNum = message.substring(message.indexOf(" ") +1);
@@ -52,6 +62,14 @@ public class ServerHandler implements Runnable {
                         chatLobby.exit(chatUser);
                     }
                 } else { // 방안에 있을 경우
+
+                    /**
+                     * 방에 입장 했을 때 알려주는 문구와 명령어를 알려주는 문구를 추가하면 좋을 것 같습니다.
+                     *
+                     * 방에서 나가는 메소드를 만들면 좋을 것 같습니다.
+                     * 방에서 강퇴시키면 강퇴당한 유저가 방의 유저 list에서 제외되나요?
+                     *
+                     */
                     if (message.indexOf("/whisper") == 0) {
                         String[] s = message.split(" ");
                         String whisperTo = s[1];
@@ -72,6 +90,7 @@ public class ServerHandler implements Runnable {
                             for (ChatUser cu : chatUsers) {
                                 if (cu.getNickname().equals(kickWho)) {
                                     cu.close();
+                                    // 방에서 사람을 강퇴시킨 다음에 방의 유저목록에서 사람을 삭제하는 메소드가 필요해보입니다.
                                 }else if(chatUser.getNickname().equals(kickWho)){
                                     chatUser.write("자기자신은 강퇴시킬 수 없습니다.");
                                 }else{
